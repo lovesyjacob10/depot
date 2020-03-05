e571c2d13e864a5e2a01071016d2a813d8e258aa
Lovesy Jacob <user@NIPLBANB1CO09.revenuemed.com>
2020-03-05 16:09:02 +0530

    updations

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -77,7 +77,7 @@ class OrdersController < ApplicationController
       params.require(:order).permit(:name, :address, :email, :pay_type)
     end
 
-    def ensure_cart_isnt_empty
+    def ensure_cart_isnt_empty    
       if @cart.line_items.empty?
         redirect_to store_index_url, notice: 'Your cart is empty'
       end
